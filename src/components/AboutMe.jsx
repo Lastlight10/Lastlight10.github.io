993a38736fa264104c769c1d91e594d7ec326d17
Lastlight10 <108103857+Lastlight10@users.noreply.github.com>
2026-05-23 16:58:36 +0800

    added details for AboutMe component
    added education and why web development

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,82 @@
+import '../assets/css/AboutMe.css'
+
+function AboutMe() {
+  return (
+    <>
+      <div className="flex-col grid grid-rows-1 gap-2 p-4 mt-48">
+        <div className="panels">
+          <h1 className="headers">Want to know more about me?</h1>
+          <div className="contentPanel justify-center">
+            <div className="row-span-1 grid grid-cols-3 text-primary-50">
+              <img className="aboutMePics" src="src/assets/profile.jpg" alt="My profile image."></img>
+              <img className="aboutMePics" src="src/assets/profile.jpg" alt="My profile image."></img>
+              <img className="aboutMePics" src="src/assets/profile.jpg" alt="My profile image."></img>
+            </div>
+            <div className=" ms-8 me-8">
+              <p className="contentText">
+                First let me reintroduce myself. My name is Clarenz Anthony Lunar Recon, a fresh graduate of Bachelor of Science in Computer Science in The University of Manila. I aim to be a professional Fullstack Web Developer specializing in Laravel Framework with ReactJS Library combined with PostgreSQL for database management. Below lies my personal information where you can contact me through email, or Facebook Messenger or you can take a look at my GitHub repositories for my projects.
+              </p>
+            </div>
+          </div>
+
+          <h1 className="headers2">Educational Background</h1>
+          <div className="contentPanel justify-center">
+            <div className="ms-8 me-8">
+               <p className="contentText">
+                My educational background has a large effect on the career path I have chosen which is to be a web developer along with the my peers and professors. These background are as follows:
+               </p>
+               <div className="grid grid-rows-3 mt-4 p-4">
+
+                <div className="row-span-1">
+                  <h3 className="headers3">
+                    Junior High School (Grade 7-10)
+                  </h3>
+                  <p className="contentSubText">
+                    2016-2020: Batasan Hills National High School (STE)
+                  </p>
+                </div>
+
+                <div className="row-span-1">
+                  <h3 className="headers3">
+                    Senior High School (Grade 11-12)
+                  </h3>
+                  <p className="contentSubText">
+                     2021-2022: Batasan Hills National High School (STEM)
+                  </p>
+                </div>
+                
+                <div className="row-span-1">
+                  <h3 className="headers3">
+                    College
+                  </h3>
+                  <p className="contentSubText">
+                    2022-2026: The University of Manila (Bachelor of Science in Computer Science)
+                  </p>
+                </div>
+               </div>
+               
+            </div>
+           
+          </div>
+
+          <h1 className="headers2">Why I Chose This Career</h1>
+          <div classname="contentPanel justify-center">
+            <div className="ms-8 me-8">
+              <p className="contentText">
+                When you ask people about what they do on the internet, most of the time they will tell you the websites they spend most of the time in like YouTube, Facebook or Instagram. These websites are a product of web developers who worked hard in maintaning and creating these websites that serves millions of users worldwide. As a fresh and upcoming web developer, I also aim to create websites popular and used by many internet users.
+              </p>
+              <p className="contentText">
+                But there is something that I haven't really paid much attention to until I developed web applications myself and that is web applications can be an alternative to traditional softwares such as an inventory management system that can be accessed on using a web browser. This makes the systems accessible to almost any web browser at any places. This is one of the things I loved about websites, their accessibility with the limits of being contained in a single computer like a software only downloaded in one computer. One other thing is the design and aesthethics of websites along with themes and animations make it very appealing to users. Web developers has a lot of tools like TailWindCSS that can help in making websites more modern and pleasing to look at.
+              </p>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      
+
+    </>
+  )
+}
+
+export default AboutMe
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,37 +2,57 @@ import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '../assets/vite.svg'
 import heroImg from '../assets/hero.png'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import '../assets/css/App.css'
 import Home from './Home.jsx'
+import AboutMe from './AboutMe.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="flex w-full bg-primary-800 mb-16 fixed top-0">
-        {/* Navbar */}
-        <div className="grid grid-cols-2 w-screen h-20">
-          <div className="welcomeHeader text-primary-50">
-            <h1 className="">WELCOME TO MY PORTFOLIO</h1>
-          </div>
-          <div className="self-center-safe justify-items-end-safe h-20">
-            <button className="navbarButtons">
-              Home Page
-            </button >
-            <button className="navbarButtons">
-              About Me
-            </button>
-            <button className="navbarButtons">
-              Projects
-            </button>
-            <button className="navbarButtons">
-              Development Stack
-            </button >
-          </div>
-        </div> 
-      </div>
-      <Home />
+      <BrowserRouter>
+        <div className="flex w-full bg-primary-800 mb-16 fixed top-0">
+          {/* Navbar */}
+          <div className="grid grid-cols-3 w-screen h-20">
+            <div className="welcomeHeader col-span-1">
+              <h1 className="text-primary-50">WELCOME TO MY PORTFOLIO</h1>
+            </div>
+            <div className="self-center-safe grid grid-cols-4 col-span-2 me-4">
+              <div className="col-span-1">
+                <a className="navbarButtons" href='/'>
+                  Home Page
+                </a >
+              </div>
+
+              <div className="col-span-1">
+                <a className="navbarButtons" href='/about_me'>
+                  About Me
+                </a>
+              </div>
+
+              <div className="col-span-1">
+                <a className="navbarButtons">
+                  Projects
+                </a>
+              </div>
+
+              <div className="col-span-1">
+                <a className="navbarButtons">
+                  Development Stack
+                </a >
+              </div>
+
+             
+            </div>
+          </div> 
+        </div>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path='/about_me' element={<AboutMe />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
